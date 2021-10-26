c330ae1fca46cbc655858eb41a067b183b6508a7
Bennett Elder <belder@hushmail.com>
2021-10-26 02:11:53 -0400

    Save that Bing URL for later

--- pages/index.js
+++ pages/index.js
@@ -48,6 +48,7 @@ export default function Home2() {
 
   // https://duckduckgo.com/?q=
   // https://www.google.com/search?q=
+  // https://www.bing.com/search?q=
 
   const updateQuery = (event) => {
     setQuery(event.target.value);
