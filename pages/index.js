ccec7bce360e7a484e4fedc844de16830b927c95
Bennett Elder <bennett-elder@users.noreply.github.com>
2021-10-26 03:18:59 -0400

    Add option for Bing or Duck and quick search examples

--- pages/index.js
+++ pages/index.js
@@ -54,6 +54,18 @@ export default function Home2() {
     setQuery(event.target.value);
   };
 
+  const setToDuck = (event) => {
+    setSearchEngine("https://duckduckgo.com/?q=")
+  }
+  
+  const setToBing = (event) => {
+    setSearchEngine("https://www.bing.com/search?q=")
+  }
+
+  const setFilterValue = (event) => {
+    setQuery(event.target.value);
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -78,6 +90,17 @@ export default function Home2() {
           <label className={styles.searchFilter} htmlFor="searchFilter">Search filter:</label>&nbsp;
           <input className={styles.searchFilter} id="searchFilter" size="30"
             onChange={ updateQuery } value={query} placeholder="remote developer relations" />
+        </p>
+
+        <p>
+          <button className={styles.searchFilterTitle} onClick={ setFilterValue } value='remote developer relations'>remote developer relations</button>
+          <button className={styles.searchFilterTitle} onClick={ setFilterValue } value='remote devops engineer'>remote devops engineer</button>
+          <button className={styles.searchFilterTitle} onClick={ setFilterValue } value='remote product manager'>remote product manager</button>
+        </p>
+
+        <p>
+          <button className={styles.searchEngine} onClick={ setToDuck }>Use Duck</button>
+          <button className={styles.searchEngine} onClick={ setToBing }>Use Bing</button>
         </p>
 
         <div className={styles.grid}>
